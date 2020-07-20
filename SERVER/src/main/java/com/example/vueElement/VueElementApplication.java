bb051e5c18394d82181074bdab88e5d32b06228a
yukai <godwopapa@163.com>
2020-07-20 22:44:09 +0800

    SERVER: first commit

---
+++ SERVER/src/main/java/com/example/vueElement/VueElementApplication.java
@@ -0,0 +1,13 @@
+package com.example.vueElement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VueElementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VueElementApplication.class, args);
+    }
+
+}
---
+++ SERVER/src/test/java/com/example/vueElement/VueElementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vueElement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VueElementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
